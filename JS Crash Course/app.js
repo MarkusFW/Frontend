f444a67bbf583ecda4be49ba4df75a79a441e048
MarkusFW <markusfw02@gmail.com>
2022-11-22 22:21:33 +0100

    makeing a function calling argument into an object

--- JS Crash Course/app.js
+++ JS Crash Course/app.js
@@ -1,31 +1,36 @@
-
-let users = [{
+let users = [
+  {
     username: `Markus`,
     email: `markus@gmail.com`,
-    password: `test123`, 
+    password: `test123`,
     subscirptionStatus: `VIP`,
     discordId: `Markus FW#0001`,
-    lessonsCompleted: [0, 1]
-},  
-{
+    lessonsCompleted: [0, 1],
+  },
+  {
     username: `Allu`,
     email: `Allu@gmail.com`,
-    password: `allu123`, 
+    password: `allu123`,
     subscirptionStatus: `VIP`,
     discordId: `Allu FW#0002`,
-    lessonsCompleted: [0, 1, 2 , 3]
-}]
+    lessonsCompleted: [0, 1, 2, 3],
+  },
+];
 
-function login(email, password) {
-    for (let i = 0; i < users.length; ++i) {
-        
-        if (users[i].email === email) {
-            console.log(email, password)
-            if (users[i].password === password) {
-                console.log(`Log the user in - the details are correct`)
-            }
-        }
-    }
+
+
+
+function register(potet) {
+    users.push(potet)
 }
 
-login(`Allu@gmail.com`, `allu123`)
+register({
+  username: `potet`,
+  email: `potet@gmail.com`,
+  password: `potet123`,
+  subscirptionStatus: `VIP`,
+  discordId: `Potet PP#0001`,
+  lessonsCompleted: [0, 1],
+});
+
+console.log(users)
