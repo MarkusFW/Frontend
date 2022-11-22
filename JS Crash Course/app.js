957a2235d9e4a819a9845bcbdea975f165d93e55
MarkusFW <markusfw02@gmail.com>
2022-11-22 19:54:33 +0100

    Object in array

--- JS Crash Course/app.js
+++ JS Crash Course/app.js
@@ -1,14 +1,17 @@
 
-let user = {
+let users = [{
     username: `Markus`,
     Email: `markus@gmail.com`,
     subscirptionStatus: `VIP`,
     discordId: `Markus FW#0001`,
     lessonsCompleted: [0, 1]
-}
+},  
+{
+    username: `Allu`,
+    Email: `Allu@gmail.com`,
+    subscirptionStatus: `VIP`,
+    discordId: `Allu FW#0002`,
+    lessonsCompleted: [0, 1, 2 , 3]
+}]
 
-console.log(user.username)
-
-console.log(user.subscirptionStatus)
-
-console.log(user.lessonsCompleted.map(element => element * 2))
+console.log(users[1])
