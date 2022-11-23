7e95166afcc30e3499619cd368236c428741b355
MarkusFW <markusfw02@gmail.com>
2022-11-23 18:32:46 +0100

    Promises

--- JS Crash Course/app.js
+++ JS Crash Course/app.js
@@ -1,36 +1,3 @@
-let users = [
-  {
-    username: `Markus`,
-    email: `markus@gmail.com`,
-    password: `test123`,
-    subscirptionStatus: `VIP`,
-    discordId: `Markus FW#0001`,
-    lessonsCompleted: [0, 1],
-  },
-  {
-    username: `Allu`,
-    email: `Allu@gmail.com`,
-    password: `allu123`,
-    subscirptionStatus: `VIP`,
-    discordId: `Allu FW#0002`,
-    lessonsCompleted: [0, 1, 2, 3],
-  },
-];
-
-
-
-
-function register(potet) {
-    users.push(potet)
+function toggleDarkMode () {
+    document.querySelector(`body`).classList.toggle(`dark-theme`)
 }
-
-register({
-  username: `potet`,
-  email: `potet@gmail.com`,
-  password: `potet123`,
-  subscirptionStatus: `VIP`,
-  discordId: `Potet PP#0001`,
-  lessonsCompleted: [0, 1],
-});
-
-console.log(users)
