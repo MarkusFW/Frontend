73c20d81100aa2f99e67032ece028c8a075d183f
MarkusFW <markusfw02@gmail.com>
2022-11-25 20:37:48 +0100

    Array elements truthy or falsy with map and ternary operator

--- JS Medium Challanges/app.js
+++ JS Medium Challanges/app.js
@@ -1,14 +1,6 @@
 
 function yoo (array) {
-    let unFalse = []
-    for (let i = 0; i < array.length; ++i) {
-        if (!!array[i] === true) {
-            unFalse.push(true)
-        }
-        else if (!!array[i] === false)
-            unFalse.push(false)
-    }
-    return unFalse
+    return array.map (elem => !!elem ? true : false)
 }
 
 console.log(yoo([`banana`, `apple`, 0, ``, 100]))
