66af9570b0ae49a89fc85dd2e8c23e730042110c
MarkusFW <markusfw02@gmail.com>
2022-12-13 22:04:41 +0100

    Add files via upload

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import User from "../Components/User.jsx";
 
 function Home() {
-    async function fetchUsers() {
-        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
-        console.log(data)
-    }
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        fetchUsers()
-    }, [])
-    return <h1>Home</h1>
+  async function fetchUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchUsers();
+    }, 500);
+  }, []);
+
+  const pixles = "3px";
+
+  return (
+    <div>
+      {users.map((user) => (
+        <User
+          key={user.id}   
+          id={user.id}
+          name={user.name}
+          email={user.email}
+          username={user.username}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
