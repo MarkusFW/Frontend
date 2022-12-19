6d5fb9e52faa528351c5794dd16eb3e267f60821
MarkusFW <markusfw02@gmail.com>
2022-12-19 02:02:51 +0100

    React E-commerce completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,54 +1,66 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import LibraryLogo from '../assets/Library.svg'
+import LibraryLogo from "../assets/Library.svg";
+import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = ({ numberOfItems }) => {
+  function openMenu() {
+    document.body.classList += " menu--open";
+  }
+
+  function closeMenu() {
+    document.body.classList.remove("menu--open");
+  }
+
   return (
     <div>
       <div className="nav__container">
-        <a href="/">
+        <Link to="/">
           <img src={LibraryLogo} alt="" className="logo" />
-        </a>
+        </Link>
         <ul className="nav__links">
-            <li className="nav__list">
-                <a href="/" className="nav__link">
-                    Home
-                </a>
-            </li>
-            <li className="nav__list">
-                <a href="/" className="nav__link">
-                    Books
-                </a>
-            </li>
-            <button className="btn__menu">
-                <FontAwesomeIcon icon="bars" />
-            </button>
-            <li className="nav__icon">
-              <a href="/cart" className="nav__link">
-                <FontAwesomeIcon icon="shopping-cart" />
-              </a>
-              <spand className="cart__length">2</spand>
-            </li>
+          <li className="nav__list">
+            <Link to="/" className="nav__link">
+              Home
+            </Link>
+          </li>
+          <li className="nav__list">
+            <Link to="/books" className="nav__link">
+              Books
+            </Link>
+          </li>
+          <button className="btn__menu" onClick={openMenu}>
+            <FontAwesomeIcon icon="bars" />
+          </button>
+          <li className="nav__icon">
+            <Link to="/cart" className="nav__link">
+              <FontAwesomeIcon icon="shopping-cart" />
+            </Link>
+            {
+              numberOfItems > 0 && <span className="cart__length">{numberOfItems}</span>
+            }
+            
+          </li>
         </ul>
         <div className="menu__backdrop">
-          <button className="btn__menu btn__menu--close" >
+          <button className="btn__menu btn__menu--close" onClick={closeMenu}>
             <FontAwesomeIcon icon="times" />
           </button>
           <ul className="menu__links">
             <li className="menu__list">
-              <a href="/" className="menu__link">
+              <Link to="/" className="menu__link">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="menu__list">
-              <a href="/books" className="menu__link">
+              <Link to="/books" className="menu__link">
                 Books
-              </a>
+              </Link>
             </li>
             <li className="menu__list">
-              <a href="/cart" className="menu__link">
+              <Link to="/cart" className="menu__link">
                 Cart
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
